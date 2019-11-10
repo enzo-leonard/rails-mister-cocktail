630de10d15c6e7810b59ec5ca5ca9f259599a817
enzo leonard <enzoleonard96@gmail.com>
2019-11-10 19:32:08 +0100

    "update"

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -3,7 +3,41 @@ class CocktailsController < ApplicationController
   before_action :find_cocktail, only: [ :show, :destroy, :edit, :update]
 
   def index
-    @cocktails = Cocktail.all.order(:id)
+    if params[:get]
+      if params[:get][:ig].size < 3
+        ig_id = params[:get][:ig][1]
+        ig = Ingredient.find(ig_id)
+        sql = "ingredient_id = #{ig_id}"
+        @cocktails = Cocktail.joins(:ingredients).where(sql)
+        @result_msg = "Liste des cocktails avec : #{ig.name} "
+      else
+
+        array_id = []
+        params[:get][:ig].each { |id| array_id << id if id!=''}
+        array_name = []
+        array_id.each { |id| array_name << Ingredient.find(id).name}
+        sql = "doses.ingredient_id = #{array_id[0]} "
+
+        array_id.each do |id|
+          sql+= "and doses.ingredient_id = #{id} "
+        end
+
+        s = " doses.ingredient_id = 2074 and doses.ingredient_id = 2097 "
+
+        #mojito = 1268
+        ig1 = Cocktail.joins(:ingredients).where("ingredient_id = 2074").map {|x| x.name }
+        ig2 = Cocktail.joins(:ingredients).where("ingredient_id = 2097").map {|x| x.name }
+        ig1 & ig2
+        #@result_msg = "Voici les cocktails que tu peux faire avec : #{array_name} "
+        @result_msg = sql
+        @cocktails = Cocktail.joins(:doses).where(s)
+      end
+
+      @cocktails = Cocktail.where(id: params[:get][:name]) if params[:get][:name].present?
+    else
+      @cocktails = Cocktail.all.order(:id)
+      @result_msg = "Liste de tout les cocktails"
+    end
   end
 
   def new
